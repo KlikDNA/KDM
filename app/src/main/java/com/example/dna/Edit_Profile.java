aa528f32bc81994283a1cbcb9147278741b9e3df
wsatrioadi <60335996+wsatrioadi@users.noreply.github.com>
2020-01-28 10:57:15 +0700

    Test
    aa

---
+++ app/src/main/java/com/example/dna/Edit_Profile.java
@@ -0,0 +1,44 @@
+package com.example.dna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Edit_Profile extends AppCompatActivity {
+        Button btn_simpan;
+        TextView btnskip;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit__profile);
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+//        }
+
+        btn_simpan = findViewById(R.id.btn_simpan);
+        btnskip = findViewById(R.id.btnskip);
+
+
+        btn_simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent success = new Intent(Edit_Profile.this,Success_regisAct.class);
+                startActivity(success);
+            }
+        });
+
+        btnskip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent skip = new Intent(Edit_Profile.this,Success_regisAct.class);
+                startActivity(skip);
+            }
+        });
+    }
+}
